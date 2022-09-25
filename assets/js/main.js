70ba6a304030c79ffb0e3022573e67ef0f86118c
Giulyano Santos <giulyanofelipesantos@gmail.com>
2022-09-24 23:26:22 -0300

    Update html, css and js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,57 +1,68 @@
-
+const telaCalc = document.querySelector('.display')
+const calcCorpo = document.querySelector('.calculadora')
 
 function calculadora() {
-  const telaCalc = document.querySelector('.display');
-  const calcCorpo = document.querySelector('.calculadora');
+  mostraDisplay()
+  limpaTela()
+  apagaUltimo()
+  realizaConta()
+  enterPress()
 
+  function realizaConta() {
+    calcCorpo.addEventListener('click', e => {
+      const elemento = e.target
 
-  mostraDisplay();
-  limpaTela();
-  apagaUltimo();
-  realizaConta();
+      try {
+        if (!telaCalc.value) {
+          return
+        }
 
+        if (elemento.classList.contains('btn-igual')) {
+          telaCalc.value = eval(telaCalc.value)
+        }
+      } catch (error) {
+        limpaTela()
+      }
+    })
+  }
 
-   function realizaConta() {
-   calcCorpo.addEventListener('click', (e)=> {
-   const elemento = e.target;
-   
-   if(elemento.classList.contains('btn-igual')){
-    telaCalc.value = eval(telaCalc.value);
-   } })
-
-   };
-
+  function enterPress() {
+    telaCalc.addEventListener('keypress', e => {
+      if (e.keyCode === 13) {
+        telaCalc.value = eval(telaCalc.value)
+      }
+    })
+  }
 
   function mostraDisplay() {
     calcCorpo.addEventListener('click', e => {
-      const elemento = e.target;
+      const elemento = e.target
+      telaCalc.focus()
+
       if (elemento.classList.contains('btn-num')) {
         telaCalc.value += elemento.textContent
       }
-    });
-  };
-
+    })
+  }
 
   function limpaTela() {
-    calcCorpo.addEventListener('click', (e)=>{
-     const elemento = e.target;
-     if(elemento.classList.contains("btn-limpar")) {
-      telaCalc.value = '';
-     }
-    });
-  };
-  
-  function apagaUltimo() {
-    calcCorpo.addEventListener('click', (e)=>{
-      let elemento = e.target;
-      if(elemento.classList.contains('btn-apagar')){
-        telaCalc.value = telaCalc.value.slice(0,-1);
+    telaCalc.value = ''
+    calcCorpo.addEventListener('click', e => {
+      const elemento = e.target
+      if (elemento.classList.contains('btn-limpar')) {
+        telaCalc.value = ''
       }
-    });
-  };
+    })
+  }
 
-
-
+  function apagaUltimo() {
+    calcCorpo.addEventListener('click', e => {
+      let elemento = e.target
+      if (elemento.classList.contains('btn-apagar')) {
+        telaCalc.value = telaCalc.value.slice(0, -1)
+      }
+    })
+  }
 }
 
-calculadora();
+calculadora()
